c544317b7911483d69713340afd518111e71caca
HAndradeSJ <henriqueandradesj@outlook.com>
2023-04-05 18:07:52 -0300

    form version

--- generator/src/pages/modelo1/index.jsx
+++ generator/src/pages/modelo1/index.jsx
@@ -1,12 +1,15 @@
 // Importações
 import React,{useState,useEffect} from 'react';
 import Navbar from '../../components/navabar';
+// importando estilos
+import {Title} from './style'
 
 // Aplicação
 const Modelo1 = ()=>{
     return(
         <>
             <Navbar/>
+            <Title>Prencha o formulario</Title>
             
         </>
     )
---
+++ generator/src/pages/modelo1/style.js
@@ -0,0 +1,9 @@
+// importação do styled
+import styled from "styled-components";
+
+export const Title = styled.h1`
+    text-align:left;
+    margin-left:4vw;
+    margin-top:15vh;
+    font-size:1.80rem;
+`
