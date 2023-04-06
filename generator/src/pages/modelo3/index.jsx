63d1c94b779b3fad2d29d28ef6c4ac8f87f887a3
HAndradeSJ <henriqueandradesj@outlook.com>
2023-04-06 15:46:39 -0300

    view complete

--- generator/src/pages/modelo3/index.jsx
+++ generator/src/pages/modelo3/index.jsx
@@ -1,12 +1,41 @@
 // Importações
 import React from 'react';
 import Navbar from '../../components/navabar';
+// importando os estilos
+import {Title, Contanier,Label,Input,Textarea,Btngenerete,Box} from './style'
 
 // Aplicação
 const Modelo3 = ()=>{
     return(
         <>
-             <Navbar/>
+            <Navbar/>
+            <Title>Prencha o Formulário</Title>
+            <Contanier>
+                {/* Formulario da pagina */}
+                <Box>
+                    <Label>Nome do Arquivo</Label>
+                    <Input type="text" name="arquivo" />
+                </Box>
+                <Box>
+                    <Label>Nome do Autor</Label>
+                    <Input type="text" name="autor"/>
+                </Box>
+                <Box>
+                    <Label>Titulo</Label>
+                    <Input type="text" name="titulo"/>
+                </Box>
+                <Box>
+                    <Label>Tabel</Label>
+                    <Input type="number" name="horizontal" placeholder='Linha Horizontal'/>
+                    <br></br>
+                    <Input type="number" name="vertical" placeholder='Linah Vertical'/>
+                </Box>
+                <Box>
+                    <Label>Texto</Label>
+                    <Textarea name="text" rows="6" cols="6"></Textarea>
+                </Box>
+                <Btngenerete>Gerar Documento</Btngenerete>
+            </Contanier>
         </>
     )
 }
---
+++ generator/src/pages/modelo3/style.js
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+    text-align:left;
+    margin-left:5vw;
+    margin-top:7vh;
+    font-size:1.50rem;
+`
+export const Contanier = styled.div`
+    display:flex;
+    flex-direction:column;
+    justify-content:left;
+    width:60vw;
+    align-items:left;
+    margin-left:4vw;
+    margin-top:3vh;
+`
+export const Label = styled.label`
+    font-size: 100%;
+    color: rgb(100, 100, 100);
+    font-weight: bold;
+    margin-left: 1vh;
+`
+export const  Input = styled.input`
+    font-size: 100%;
+    padding: 0.8em;
+    outline: none;
+    border: 2px solid rgb(200, 200, 200);
+    background-color: transparent;
+    border-radius: 12px;
+    width:50%;
+    font-weight: bold;
+    height:6vh;
+    cursor: pointer;
+    
+`
+export const Textarea = styled.textarea`
+    font-size: 100%;
+    padding: 0.8em;
+    outline: none;
+    border: 2px solid rgb(200, 200, 200);
+    background-color: transparent;
+    border-radius: 12px;
+    width:50%;
+    font-weight: bold;
+    height:8vh;
+    cursor: pointer;
+    
+
+`
+export const Btngenerete = styled.button`  
+    transition: all 0.2s ease-in;
+    margin-top:2vh;
+    margin-left:0.50vw;
+    width:25%;
+    color: white;
+    padding: 0.7em 1.7em;]
+    font-weight: bold;
+    font-size: 18px;
+    border-radius: 0.5em;
+    background:#212529;
+    border: 1px solid #e8e8e8;
+    box-shadow: 6px 6px 12px #c5c5c5,
+            -6px -6px 12px #ffffff;
+    cursor: pointer;
+
+`
+
+export const Box = styled.div`
+    padding:8px;
+    display:flex;
+    flex-direction: column;
+`
